0362e745af32321d92326296e7c022af6eccd9b7
Iansus <jean.iansus@gmail.com>
2016-02-16 13:38:26 +0100

    Updated binsearch GET parameters

--- views/serie.view.php
+++ views/serie.view.php
@@ -51,7 +51,7 @@
 			<tr>
 				<td>S<?php echo $se; ?>E<?php echo $epstr ?></td>
 				<td><a href="<?php echo $ep->get('link'); ?>" target="_blank"><img src="<?php echo $_G['SERVER_ROOT']; ?>static/img/extern.svg" /></a></td>
-				<td><a href="http://binsearch.info/index.php?&m=&max=25&adv_g=&adv_age=999&adv_sort=date&minsize=200&maxsize=&font=&postdate=&q=<?php echo ($bs); ?>" target="_blank"><img src="<?php echo $_G['SERVER_ROOT']; ?>static/img/extern.svg" /></a></td>
+				<td><a href="http://binsearch.info/index.php?&m=&max=25&adv_g=&adv_age=999&adv_sort=date&xminsize=200&maxsize=&font=&postdate=&q=<?php echo ($bs); ?>" target="_blank"><img src="<?php echo $_G['SERVER_ROOT']; ?>static/img/extern.svg" /></a></td>
 				<td><input type="radio" onClick="location.href='set-last-seen.php?id=<?php echo $ep->get('id'); ?>'" /></td>
 				<td><a style="color:white" name="<?php echo 's'.$seno.'e'.$epno; ?>"><?php echo $airDate; ?></a></td>
 				<td style="text-align:left"><span class="<?php echo $status; ?>"><?php Functions::echos($ep->get('name')); ?></span></td>
