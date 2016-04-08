dbe6b7a084d2c63be5e62751367e6e9fecdc7791
Iansus <jean.iansus@gmail.com>
2016-04-08 21:05:29 +0200

    Multi user is here !

--- database.sql
+++ database.sql
@@ -22,25 +22,6 @@
 CREATE DATABASE /*!32312 IF NOT EXISTS*/ `check_series` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
 USE `check_series`;
---
--- Table structure for table `series`
---
-
-DROP TABLE IF EXISTS `series`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `series` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `epguides_url` varchar(255) NOT NULL,
-  `binsearch_url` varchar(255) NOT NULL,
-  `dpstream_id` int(11) NOT NULL,
-  `last_seen_season` int(11) NOT NULL,
-  `last_seen_episode` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 --
 -- Table structure for table `episodes`
@@ -60,7 +41,74 @@ CREATE TABLE `episodes` (
   PRIMARY KEY (`id`),
   KEY `serie_id` (`serie_id`),
   CONSTRAINT `to_series` FOREIGN KEY (`serie_id`) REFERENCES `series` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3027 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3046 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `series`
+--
+
+DROP TABLE IF EXISTS `series`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `series` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `epguides_url` varchar(255) NOT NULL,
+  `binsearch_url` varchar(255) NOT NULL,
+  `dpstream_id` varchar(511) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `store_iv` varchar(88) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+
+DROP TABLE IF EXISTS `tokens`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tokens` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
+  `value` varchar(44) NOT NULL,
+  `last_activity` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id to user.id` (`user_id`),
+  CONSTRAINT `user_id to user.id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+DROP TABLE IF EXISTS `user_series`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_series` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
+  `serie_id` int(11) NOT NULL,
+  `last_seen_season` int(11) NOT NULL DEFAULT '0',
+  `last_seen_episode` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `user_id_to_users` (`user_id`),
+  KEY `serie_id_to_series` (`serie_id`),
+  CONSTRAINT `serie_id_to_series` FOREIGN KEY (`serie_id`) REFERENCES `series` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `user_id_to_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 
@@ -72,4 +120,4 @@ CREATE TABLE `episodes` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-03-27 13:02:01
+-- Dump completed on 2016-04-08 21:04:26
